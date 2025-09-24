ffcb5718612a521e34757c97da7a202056bb7541
puremjlee <mj.lee@snowflake.com>
2025-09-24 14:00:07 +0000

    add data loading script

---
+++ 01.data_catalog/data-load.sql
@@ -0,0 +1,47 @@
+
+create database demo;
+create schema magi_handson;
+
+-- use demo.magi_handson;
+use warehouse compute_wh;
+
+// external stage (s3) 생성
+CREATE STAGE ext_stg
+URL='s3://sfworkshop-sample-data/';
+-- STORAGE_INTEGRATION = myint;
+
+// external stage (s3) 에 위치한 파일확인
+ls @ext_stg;
+
+// infer schema 를 위한 파일포맷
+create file format csv_format_read 
+type=csv
+PARSE_HEADER = true;
+
+// 데이터 적재를 위한 파일포맷
+create file format csv_format_loading
+type=csv
+skip_header =1;
+
+// 테이블 생성
+CREATE OR REPLACE TABLE SF_KOSCOM_ETF_JITRADE_DAILY
+  USING TEMPLATE (
+    SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
+    FROM TABLE(
+      INFER_SCHEMA(
+        LOCATION => '@ext_stg/SF_KOSCOM_ETF_JITRADE_DAILY.csv',
+        FILE_FORMAT => 'csv_format_read',
+        IGNORE_CASE => true
+      )
+    )
+  );
+
+// 확인
+select * from SF_KOSCOM_ETF_JITRADE_DAILY limit 10;
+
+// 적재 
+copy into SF_KOSCOM_ETF_JITRADE_DAILY
+from @ext_stg/SF_KOSCOM_ETF_JITRADE_DAILY.csv
+FILE_FORMAT = (FORMAT_NAME = 'csv_format_loading');
+
+----------------------------------------------------------------
